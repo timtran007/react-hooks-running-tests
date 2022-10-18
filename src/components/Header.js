0ec4e55e255fc1e4d8c4233ac255366c9f648083
Tim <timtran007@gmail.com>
2022-10-17 21:41:29 -0500

    commit tree fdded9ee33729747ab8482bc9a4a6d210a2a2301

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ function Header(props) {
       return (
         <h1>
           {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
+          hello from the Header!
         </h1>
-      )
+  )
 }
 
 export default Header;
